e070ab67b61d11c827e1bb9fd938eb00988efa72
Ramaun <Ramaunkoomar@gmail.com>
2022-09-11 11:10:08 -0700

    Added Product service implementation.

---
+++ src/main/java/com/genspark/productapi/service/product/ProductServiceImpl.java
@@ -0,0 +1,28 @@
+package com.genspark.productapi.service.product;
+
+import com.genspark.productapi.dao.product.ProductDao;
+import com.genspark.productapi.model.product.Product;
+import org.springframework.stereotype.Service;
+import java.util.Optional;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+
+    private ProductDao productDao;
+
+    @Override
+    public Optional<Product> getProductById(Long productId) {
+        if(productId == null) {
+            throw new IllegalArgumentException("Product ID must not be null");
+        }
+        return productDao.findById(productId);
+    }
+
+    @Override
+    public void addProduct(Product product) {
+        if(product == null) {
+            throw new IllegalArgumentException("Product cannot be null");
+        }
+        productDao.save(product);
+    }
+}
