53631861181dc2bd73060b19f21c4261f0a59815
Ramaun <Ramaunkoomar@gmail.com>
2022-09-11 11:38:23 -0700

    Annotated product id with @GeneratedValue

--- src/main/java/com/genspark/productapi/model/product/Product.java
+++ src/main/java/com/genspark/productapi/model/product/Product.java
@@ -5,6 +5,7 @@ import javax.persistence.*;
 @Entity
 public class Product {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private int price;
